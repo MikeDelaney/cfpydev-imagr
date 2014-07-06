a383b5dafed0f2af9b6269c5c8e3a61e83c854cd
eyueld-codefellow <eyueld-codfefellow@gmail.com>
2014-07-05 20:10:08 -0700

    Added test for Photo model.

--- cfpydev-imagr/imagr_images/tests.py
+++ cfpydev-imagr/imagr_images/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from imagr_images.models import Photo, Album
+from django.core.files import File
+import datetime
 
 # Create your tests here.
+
+
+class PhotoTests(TestCase):
+
+    def test_photo_creation(self):
+        img = File(open('/Users/eyuelabebe/Desktop/projects/Network_tools2/root/static/images/java.jpg'))
+        photo1 = Photo(image=img, title='test_title', description='test_description', date_uploaded= datetime.datetime.now(), privacy_option = 1)
+
+        self.assertEqual(photo1.image, img)
+        self.assertEqual(photo1.title, 'test_title')
+        self.assertEqual(photo1.description, 'test_description')
+        self.assertEqual(photo1.privacy_option, 1)
+        # self.assertEqual(photo1.date_uploaded, datetime.datetime.now())
+
+
