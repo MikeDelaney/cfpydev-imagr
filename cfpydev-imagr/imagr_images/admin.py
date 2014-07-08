bb2bf268d3c5cb23dbda61b76696d84bfe02a4e7
miramzz <mira.muazzez@gmail.com>
2014-07-08 16:22:56 -0700

    allow admin for photo/album search

--- cfpydev-imagr/imagr_images/admin.py
+++ cfpydev-imagr/imagr_images/admin.py
@@ -1,15 +1,21 @@
+import datetime
 from django.contrib import admin
 from imagr_images.models import Photo, Album, ImagrUser
 
 
+
 class PhotoAdmin(admin.ModelAdmin):
+    search_fields = ['username', 'first_name', 'last_name', 'email']
     list_display = ('title', 'user_link', 'owner', 'image_size', 'height', 'width')
     list_display_links = ('owner',)
     readonly_fields = ('date_uploaded',
-                       'date_modified',
-                       'date_published')
+           'date_modified',
+           'date_published',
+           )
+    date_hirerachy = 'date_published'
 
 class AlbumAdmin(admin.ModelAdmin):
+    search_fields = ['username', 'first_name', 'last_name', 'email']
     list_display = ('title', 'owner_link')
     readonly_fields = ('date_uploaded',
                        'date_modified',
