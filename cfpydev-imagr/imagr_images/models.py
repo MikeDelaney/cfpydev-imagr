140274f706c555c3c9793b5c789eaefb893b79d2
MikeDelaney <mdelaney@utexas.edu>
2014-07-07 18:40:19 -0700

    Build list_friends, request_friendship, accept_friendship functions

--- cfpydev-imagr/imagr_images/models.py
+++ cfpydev-imagr/imagr_images/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User, AbstractUser, UserManager
 from imagr_site import settings
+from django.db.models import Q
 
 class Photo(models.Model):
 
@@ -8,8 +9,8 @@ class Photo(models.Model):
     image_upload_folder = '/Users/eyuelabebe/Desktop/projects/django-imagr/cfpydev-imagr/imagr_images'
 
     FOLLOWING_BITS = {
-    'user_one': 1,
-    'user_two': 2
+        'user_one': 1,
+        'user_two': 2
     }
 
     FOLLOWER_STATUSES = (
@@ -20,11 +21,18 @@ class Photo(models.Model):
     )
 
     FOLLOWER_SYMBOLS = {
-    0: u' X ',
-    1: u' +->',
-    2: u'<-+ ',
-    3: u'<+-+>',
+        0: u' X ',
+        1: u' +->',
+        2: u'<-+ ',
+        3: u'<+-+>',
     }
+
+    FRIEND_STATUSES = (
+        (0, u'not friends'),
+        (1, u'user_one requesting user_two'),
+        (2, u'user_two requesting user_one'),
+        (3, u'friends'),
+    )
 
     image = models.ImageField(upload_to=image_upload_folder)
     user = models.ForeignKey(User) # we can also do user = models.ForeignKey("django.contrib.auth.models.User")
@@ -87,14 +95,43 @@ class ImagrUser(AbstractUser):
         pass
     def unfollow(self):
         pass
+
     def list_friends(self):
-        pass
-    def request_friendship(self):
-        pass
+        friends = filter(
+            (Q(relationship_from__user_one=self) &
+             Q(relationship_from__friendship__exact=True)) |
+            (Q(relationship_to__user_two=self) &
+             Q(relationship_to__friendship__exact=True))
+        )
+        return friends
+
+    def request_friendship(self, other):
+        if other not in self.list_friends():
+            rel = self._relationship_with(other)
+            if rel is not None:
+                for slot in ['user_one', 'user_two']:
+                    if getattr(rel, slot) == self:
+                        if rel.friendship not in (1, 2):
+                            bitmask = FOLLOWING_BITS[slot]
+                            rel.friendship = rel.friendship | bitmask
+                            break
+        rel.full_clean()
+        rel.save()
+
+
     def end_friendship(self):
         pass
-    def accept_friendship_request(self):
-        pass
+
+    def accept_friendship_request(self, other):
+        if other not in self.list_friends():
+            rel = self._relationship_with(other)
+            if rel is not None:
+                rel.friendship = 3
+            else:
+                rel = Relationships(user_one=self,
+                                    user_two=other,
+                                    follower_status=0,
+                                    friendship=3)
 
     def _relationship_with(self, to_user):
         relationship = None
@@ -117,8 +154,8 @@ class Relationships(models.Model):
 
     user_one = models.ForeignKey('ImagrUser', related_name='relationship_from')
     user_two = models.ForeignKey('ImagrUser', related_name='relationship_to')
-    follower_status = models.IntegerField(choices = FOLLOWER_STATUSES )
-    friendship = models.NullBooleanField(null=True, blank=True, default=None)
+    follower_status = models.IntegerField(choices=FOLLOWER_STATUSES)
+    friendship = models.IntegerField(choices=FRIEND_STATUSES)
 
 
     def __unicode__(self):
