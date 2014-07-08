198a15c3bacf693bfcbbd1da2ceef9a553557157
MikeDelaney <mdelaney@utexas.edu>
2014-07-07 23:48:00 -0700

    Add list_display & list display links to show owner for Photo & Album. Add search field for ImagrUser.

--- cfpydev-imagr/imagr_images/admin.py
+++ cfpydev-imagr/imagr_images/admin.py
@@ -3,5 +3,20 @@ from imagr_images.models import Photo, Album, ImagrUser
 
 # Register your models here.
 
-class PhotoAdmin():
-    pass
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user')
+    list_display_links = ('user',)
+
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner')
+    list_display_links = ('owner',)
+
+
+class ImagrUserAdmin(admin.ModelAdmin):
+    search_fields = ['username', 'first_name', 'last_name', 'email']
+
+admin.site.register(Photo, PhotoAdmin)
+admin.site.register(Album, AlbumAdmin)
+admin.site.register(ImagrUser, ImagrUserAdmin)
