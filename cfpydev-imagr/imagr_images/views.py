a3c86de78a7fa963d8eedabdb391e3cd2c46cec4
MikeDelaney <mdelaney@utexas.edu>
2014-07-10 16:16:11 -0700

    Create basic views for front page and home page

--- cfpydev-imagr/imagr_images/views.py
+++ cfpydev-imagr/imagr_images/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 
-# Create your views here.
+
+def front(request):
+    template = loader.get_template('imagr_images/front.html')
+    context = RequestContext(request)
+    return HttpResponse(template.render(context))
+
+
+def home(request):
+    template = loader.get_template('imagr_images/home.html')
+    context = RequestContext(request)
+    return HttpResponse(template.render(context))
