abe4ddba18757ef0aeafdca263a46553cb44fddf
MikeDelaney <mdelaney@utexas.edu>
2014-07-10 16:15:43 -0700

    Include app views in site urls

--- cfpydev-imagr/imagr_site/urls.py
+++ cfpydev-imagr/imagr_site/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^imagr_images/', include('imagr_images.urls')),
 )
