b610c883a5be75e08b38fb04d8b63ae141f725d5
eyueld-codefellow <eyueld-codfefellow@gmail.com>
2014-07-10 20:18:31 -0700

    Installed South. Migrated db to south.

--- cfpydev-imagr/imagr_site/settings.py
+++ cfpydev-imagr/imagr_site/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'south',
+    'south',
     'imagr_images',
     'imagr_user',
 )
