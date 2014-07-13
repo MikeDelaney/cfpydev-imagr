a927d3d135eb5508803f7e075cac6d0c9a5455c1
MikeDelaney <mdelaney@utexas.edu>
2014-07-12 23:52:45 -0700

    Move home view to imagr_user

--- cfpydev-imagr/imagr_images/views.py
+++ cfpydev-imagr/imagr_images/views.py
@@ -5,17 +5,6 @@ from django.template import RequestContext, loader
 from imagr_images.models import Album, Photo, ImagrUser, Relationships
 
 
-def home(request, user_id):
-    template = loader.get_template('imagr_images/home.html')
-    user = ImagrUser.objects.get(pk=user_id)
-    # probably should implement custom 404 if album list is empty
-    # use try/except with:
-    # album_list = Album.objects.filter(owner__exact=user_id)
-    album_list = get_list_or_404(Album, owner=user_id)
-    context = RequestContext(request, {'user': user,
-                                       'album_list': album_list},)
-    return HttpResponse(template.render(context))
-
 def albumView(request, album_id):
     template = loader.get_template('imagr_images/albumView.html')
     return render_to_response('imagr_images/albumView.html',
