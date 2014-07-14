8bb2234fa7c8e898dca3fa7cbef9c2f1d3deb977
eyueld-codefellow <eyueld-codfefellow@gmail.com>
2014-07-14 13:48:10 -0700

    Modefied settings to include media url and urls.py to recognize the static root files.

--- cfpydev-imagr/imagr_site/urls.py
+++ cfpydev-imagr/imagr_site/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 from imagr_site import views
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -13,3 +16,5 @@ urlpatterns = patterns('',
     url(r'^photo/', include('imagr_images.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
