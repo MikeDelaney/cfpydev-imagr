6f6e7096fbd246fcdadcd1e2cf368e08e5eaffd5
MikeDelaney <mdelaney@utexas.edu>
2014-07-13 20:17:30 -0700

    Implement link functionality

--- cfpydev-imagr/imagr_images/urls.py
+++ cfpydev-imagr/imagr_images/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from imagr_images import views
 
 urlpatterns = patterns('',
     url(r'^(?P<album_id>\d+)/$', views.albumView, name='albums'),
+    url(r'^home/', include('imagr_user.urls')),
     )
