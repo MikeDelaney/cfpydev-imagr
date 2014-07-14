3065da37b51e39f26833cd23cb650e8e54c9a627
MikeDelaney <mdelaney@utexas.edu>
2014-07-13 20:18:37 -0700

    Clean up home view function

--- cfpydev-imagr/imagr_user/views.py
+++ cfpydev-imagr/imagr_user/views.py
@@ -1,17 +1,12 @@
-from django.http import HttpResponse
-from django.template import RequestContext, loader
-from django.shortcuts import get_list_or_404
-from imagr_images.models import Album, Photo
+from django.template import RequestContext
+from django.shortcuts import get_list_or_404, render
+from imagr_images.models import Album
 from imagr_user.models import ImagrUser
 
 
 def home(request, user_id):
-    template = loader.get_template('imagr_images/home.html')
     user = ImagrUser.objects.get(pk=user_id)
-    # probably should implement custom 404 if album list is empty
-    # use try/except with:
-    # album_list = Album.objects.filter(owner__exact=user_id)
     album_list = get_list_or_404(Album, owner=user_id)
     context = RequestContext(request, {'user': user,
                                        'album_list': album_list},)
-    return HttpResponse(template.render(context))
+    return render(request, 'imagr_images/home.html', context)
