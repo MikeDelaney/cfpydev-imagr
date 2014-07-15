fb04c239faefbbe03877358cc87db9d4fab3ff42
MikeDelaney <mdelaney@utexas.edu>
2014-07-15 12:32:20 -0700

    First run at view testing for user home page

--- cfpydev-imagr/imagr_user/tests.py
+++ cfpydev-imagr/imagr_user/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from imagr_user.models import ImagrUser
+from imagr_images.models import Photo, Album
+from django.test.client import Client
+from django.db import models
 
 
 class ImagrUser_Relations_Test(TestCase):
@@ -89,3 +92,61 @@ class ImagrUser_Relations_Test(TestCase):
         self.assertEqual(list(usr1_list_friends), [self.usr2, self.usr3])
         self.assertEqual(list(usr2_list_friends), [self.usr1, self.usr3])
         self.assertEqual(list(usr3_list_friends), [self.usr1, self.usr2])
+
+
+class Home_test(TestCase):
+    def setUp(self):
+        # create users
+        self.usr1 = ImagrUser.objects.create(first_name='Eyuel',
+                                             last_name='Abebe',
+                                             username='Eyuel')
+        self.usr2 = ImagrUser.objects.create(first_name='Muazzez',
+                                             last_name='Mira',
+                                             username='Muazzez')
+        # create photos
+        photo1 = Photo.objects.create(title="7fTNh.jpg",
+                                      description="photo1_descr",
+                                      owner=self.usr1,
+                                      image=models.ImageField(upload_to='/static/front_img',
+                                                              height_field=279,
+                                                              width_field=400))
+        photo2 = Photo.objects.create(title="beer.jpg",
+                                      description="photo2_descr",
+                                      owner=self.usr1,
+                                      image=models.ImageField(upload_to='/static/front_img'))
+        # photo3 = Photo.objects.create(title="photo3",
+        #                               description="photo3_descr",
+        #                               owner=self.usr2)
+        # photo4 = Photo.objects.create(title="photo4",
+        #                               description="photo4_descr",
+        #                               owner=self.usr2)
+        # create albums
+        album1 = Album.objects.create(owner=self.usr1,
+                                      title="album1",
+                                      description="a1_descr")
+        album2 = Album.objects.create(owner=self.usr2,
+                                      title="album2",
+                                      description="a2_descr")
+        # add photos to album1
+        album1.photos = [photo1, photo2]
+        album1.cover_photo = photo1
+        # set up test client
+
+    def tearDown(self):
+        self.usr1.delete()
+        self.usr2.delete()
+        self.photo1.delete()
+        self.photo2.delete()
+        self.photo3.delete()
+        self.photo4.delete()
+        self.album1.delete()
+
+    def test_usr1_response(self):
+        c = Client()
+        response = c.get('/home/1')
+        self.assertEqual(response.status_code, 301)
+
+    def test_usr2_response(self):
+        c = Client()
+        response = c.get('/home/2')
+        self.assertEqual(response.status_code, 404)
