f5b4399a9e8057759547c3992fc49a716eba59e9
miramzz <mira.muazzez@gmail.com>
2014-07-15 16:24:55 -0700

    more coding for stream test

--- cfpydev-imagr/imagr_images/tests.py
+++ cfpydev-imagr/imagr_images/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from imagr_images.models import Photo, Album, ImagrUser, Relationships
 import datetime
+from django.test.client import Client
+from django.core.files import File
 
 
 class PhotoTests(TestCase):
@@ -26,7 +28,7 @@ class AlbumTests(TestCase):
         self.assertEqual(album1.privacy_option, 1)
 
 class StreamViewTests(TestCase):
-    def test_setUp(self):
+    def setUp(self):
         self.usr1 = ImagrUser(first_name='Eyuel', last_name='Abebe', username='Eyuel')
         self.usr2 = ImagrUser(first_name='Muazz', last_name='Mira', username='Muazz')
         self.usr3 = ImagrUser(first_name='Mike', last_name='Delany', username='Mike')
@@ -46,33 +48,64 @@ class StreamViewTests(TestCase):
         date_index = 0
         user_index = 0
         for item in photo_list:
-            Photo(title=item,
-                       description='test_description',
-                       date_uploaded=date_list[date_index],
-                       privacy_option=1,
-                       owner=user_list[user_index]
-                       )
+            with open('/Users/muazzezmira/projects/django_imagr/cfpydev-imagr/cfpydev-imagr/imagr_images/static/front_img/7fTNh.jpg', 'r') as f:
+                photo_file = File(f)
+                photo = Photo()
+                photo.image = photo_file
+                photo.title = item
+                photo.owner = user_list[user_index]
+                photo.privacy_option = 2
+                photo.date_uploaded=date_list[date_index]
+                photo.save()
             date_index +=1
             if not date_index % 5:
                 user_index += 1
+            photo.save()
 
-        import pdb;pdb.set_trace()
-        print "here we are"
-        # db den fotolar gercekten userlara yerlesmis mi bak
-        print self.usr1, "--->>",type(self.usr1)
-        print self.usr1.id
+        self.usr1.request_friendship(self.usr2)
+        self.usr2.accept_friendship_request(self.usr1)
 
+        self.usr2.request_friendship(self.usr4)
+        self.usr4.accept_friendship_request(self.usr2)
+
+        self.usr1.follow(self.usr3)
+        self.usr4.follow(self.usr1)
+        self.usr1.follow(self.usr5)
+        self.usr5.follow(self.usr1)
+
+
+    def tearDown(self):
+
+        self.usr1.delete()
+        self.usr2.delete()
+        self.usr3.delete()
+        self.usr4.delete()
+        self.usr5.delete()
+
+
+    def test_stream(self):
+            # db den fotolar gercekten userlara yerlesmis mi bak
         usr1_photos = Photo.objects.filter(owner__exact=self.usr1.id)
-        print len(usr1_photos)
+        usr2_photos = Photo.objects.filter(owner__exact=self.usr2.id)
+        usr3_photos = Photo.objects.filter(owner__exact=self.usr3.id)
+        usr4_photos = Photo.objects.filter(owner__exact=self.usr4.id)
+        usr5_photos = Photo.objects.filter(owner__exact=self.usr5.id)
         assert len(usr1_photos) == 5
+        assert len(usr2_photos) == 5
+        assert len(usr3_photos) == 5
+        assert len(usr4_photos) == 5
+        assert len(usr5_photos) == 0
 
+        c = Client()
+        response = c.get('/stream/' + str(self.usr1.id) + '/')
+        print response
+        # self.assertEqual(response.status_code, 200)
 
 
 
         # eger hersey yolundaysa:
         # userlar arasinda iliskileri kurup test caseleri yazabilirsin
         # super!
-
 
 
 
