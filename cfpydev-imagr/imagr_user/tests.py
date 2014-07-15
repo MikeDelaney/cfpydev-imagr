24cf0038376171672ad0c7bf532d16dfdaeb9b05
MikeDelaney <mdelaney@utexas.edu>
2014-07-15 14:30:23 -0700

    Implement tests for home view

--- cfpydev-imagr/imagr_user/tests.py
+++ cfpydev-imagr/imagr_user/tests.py
@@ -3,6 +3,7 @@ from imagr_user.models import ImagrUser
 from imagr_images.models import Photo, Album
 from django.test.client import Client
 from django.db import models
+from django.core.files import File
 
 
 class ImagrUser_Relations_Test(TestCase):
@@ -104,49 +105,39 @@ class Home_test(TestCase):
                                              last_name='Mira',
                                              username='Muazzez')
         # create photos
-        photo1 = Photo.objects.create(title="7fTNh.jpg",
-                                      description="photo1_descr",
-                                      owner=self.usr1,
-                                      image=models.ImageField(upload_to='/static/front_img',
-                                                              height_field=279,
-                                                              width_field=400))
-        photo2 = Photo.objects.create(title="beer.jpg",
-                                      description="photo2_descr",
-                                      owner=self.usr1,
-                                      image=models.ImageField(upload_to='/static/front_img'))
-        # photo3 = Photo.objects.create(title="photo3",
-        #                               description="photo3_descr",
-        #                               owner=self.usr2)
-        # photo4 = Photo.objects.create(title="photo4",
-        #                               description="photo4_descr",
-        #                               owner=self.usr2)
-        # create albums
+        with open('/home/miked/projects/django-project/cfpydev-imagr/cfpydev-imagr/imagr_images/static/front_img/7fTNh.jpg', 'r') as f:
+            photo_file = File(f)
+            photo1 = Photo()
+            photo1.image = photo_file
+            photo1.title = 'horse'
+            photo1.owner = self.usr1
+            photo1.privacy_option = 2
+            photo1.save()
+        with open('/home/miked/projects/django-project/cfpydev-imagr/cfpydev-imagr/imagr_images/static/front_img/beer.jpg', 'r') as f:
+            photo_file = File(f)
+            photo2 = Photo()
+            photo2.image = photo_file
+            photo2.title = 'beer'
+            photo2.owner = self.usr1
+            photo2.privacy_option = 2
+            photo2.save()
+
+        # create album
         album1 = Album.objects.create(owner=self.usr1,
                                       title="album1",
-                                      description="a1_descr")
-        album2 = Album.objects.create(owner=self.usr2,
-                                      title="album2",
-                                      description="a2_descr")
-        # add photos to album1
-        album1.photos = [photo1, photo2]
-        album1.cover_photo = photo1
-        # set up test client
+                                      description="a1_descr",
+                                      privacy_option=2)
 
     def tearDown(self):
         self.usr1.delete()
         self.usr2.delete()
-        self.photo1.delete()
-        self.photo2.delete()
-        self.photo3.delete()
-        self.photo4.delete()
-        self.album1.delete()
 
     def test_usr1_response(self):
         c = Client()
-        response = c.get('/home/1')
-        self.assertEqual(response.status_code, 301)
+        response = c.get('/home/' + str(self.usr1.id) + '/')
+        self.assertEqual(response.status_code, 200)
 
     def test_usr2_response(self):
         c = Client()
-        response = c.get('/home/2')
+        response = c.get('/home/' + str(self.usr2.id) + '/')
         self.assertEqual(response.status_code, 404)
