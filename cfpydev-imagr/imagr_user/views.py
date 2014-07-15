75790321bf62352144c7f10a88c2bab4d228af70
MikeDelaney <mdelaney@utexas.edu>
2014-07-15 14:32:06 -0700

    Fix user name issue in home view

--- cfpydev-imagr/imagr_user/views.py
+++ cfpydev-imagr/imagr_user/views.py
@@ -5,8 +5,8 @@ from imagr_user.models import ImagrUser
 
 
 def home(request, user_id):
-    user = ImagrUser.objects.get(pk=user_id)
+    user_ = ImagrUser.objects.get(pk=user_id)
     album_list = get_list_or_404(Album, owner=user_id)
-    context = RequestContext(request, {'user': user,
+    context = RequestContext(request, {'ImagrUser': user_,
                                        'album_list': album_list},)
     return render(request, 'imagr_images/home.html', context)
