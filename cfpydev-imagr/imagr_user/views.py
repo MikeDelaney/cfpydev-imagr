035a8a332e282521563e4e0a2085c2cdaf4f38f4
miramzz <mira.muazzez@gmail.com>
2014-07-30 12:06:20 -0700

    no album no photos, home page error fixed

--- cfpydev-imagr/imagr_user/views.py
+++ cfpydev-imagr/imagr_user/views.py
@@ -2,12 +2,17 @@ from django.template import RequestContext
 from django.shortcuts import get_list_or_404, render_to_response, get_object_or_404
 from imagr_images.models import Album
 from imagr_user.models import ImagrUser
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def home(request):
 
     user_ = get_object_or_404(ImagrUser, pk=request.user.id)
-    album_list = get_list_or_404(Album, owner=request.user.id)
+    #album_list = get_list_or_404(Album, owner=request.user.id)
+    try :
+        album_list = Album.objects.get(owner=request.user.id)
+    except ObjectDoesNotExist:
+        album_list=[]
     context = RequestContext(request, {'ImagrUser': user_,
                                        'album_list': album_list},)
 
