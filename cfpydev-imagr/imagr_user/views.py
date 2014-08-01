fbd4b7a21d969e6b8217f83e563a8e996303447c
MikeDelaney <mdelaney@utexas.edu>
2014-08-01 10:37:41 -0700

    Remove unused import

--- cfpydev-imagr/imagr_user/views.py
+++ cfpydev-imagr/imagr_user/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
-from django.shortcuts import get_list_or_404, render_to_response, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404
 from imagr_images.models import Album
 from imagr_user.models import ImagrUser
 
